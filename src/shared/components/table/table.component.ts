263d102a5ef132603ffbd60811b843d61884e2f1
munir-fati-haji <munirfati95@gmail.com>
2024-09-10 11:59:53 +0200

    feature/table-part-1

--- src/shared/components/table/table.component.ts
+++ src/shared/components/table/table.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { StringUtils } from '../../utils/string-utils';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, MatFormFieldModule, MatInputModule],
   templateUrl: './table.component.html',
-  styleUrl: './table.component.scss'
+  styleUrls: ['./table.component.scss'],
 })
-export class TableComponent {
+export class TableComponent<T> {
+  @Input({ required: true }) public set displayedColumns(columns: string[]) {
+    this.columns = columns;
+    this.headerMap = this.generateHeaderMap(columns);
+  }
+  @Input({ required: true }) public set rowData(data: T[]) {
+    this.dataSource = new MatTableDataSource(data)
+  };
+  public headerMap!: { [key: string]: string };
+  public columns!: string[];
+  public dataSource!: MatTableDataSource<T>;
 
+  public applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  private generateHeaderMap(columns: string[]): { [key: string]: string } {
+    return columns.reduce(
+      (acc, column) => ({
+        ...acc,
+        [column]: StringUtils.convertCamelCaseToTitleCase(column),
+      }),
+      {}
+    );
+  }
 }
---
+++ src/shared/utils/string-utils.ts
@@ -0,0 +1,20 @@
+export class StringUtils {
+    public static convertCamelCaseToTitleCase(str: string): string {
+        return str
+            .replace(/([A-Z])/g, ' $1')
+            .replace(/^./, (char) => char.toUpperCase())
+            .trim();
+    }
+
+    public static convertTitleCaseToCamelCase(str: string): string {
+        return str
+            .toLowerCase()
+            .split(' ')
+            .map((word, index) => 
+                index === 0
+                    ? word
+                    : word.charAt(0).toUpperCase() + word.slice(1)
+            )
+            .join('');
+    }
+}
